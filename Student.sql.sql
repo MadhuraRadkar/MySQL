fba93fd0c90c0c2d607ca7a57af8e22186efa6af
MadhuraRadkar <142924000+MadhuraRadkar@users.noreply.github.com>
2023-09-16 21:13:22 +0530

    Add files via upload

---
+++ Student.sql.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Student
+(
+rollno int,
+name varchar(20),
+percentage numeric(6,2),
+city varchar(10),
+dob date
+)
+
+--Modify the data type or size of data type.
+alter table Student alter column name varchar(25)
+
+-- Add column to the existing table.
+alter table Student add country varchar(20)
+
+-- Remove column from the existing table.
+alter table Student drop column country
+
+-- Rename column name.
+exec sp_rename 'Student.name','studentname'
